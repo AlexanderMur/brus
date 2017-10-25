a524734d0db11a46877c74a0e1bbd5afd7b992e0
Alexey Muravev <alvmuravev@hotmail.com>
2017-10-25 16:18:51 +0500

    work3

--- index.php
+++ index.php
@@ -28,10 +28,30 @@
                         <div class="col">
                             <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>kirov-komi-srub@mail.ru</a>
                         </div>
-                        <div class="col"></div>
-                        <div class="col"></div>
+                        <div class="col">
+                            <p class="phone-box text-right"><i class="fa fa-phone-square" aria-hidden="true"></i>8 (919) 516-00-99<br>8 (963) 551-00-99</p>
+                        </div>
+                        <div class="col">
+                            <a href="#" role="button" class="btn btn-danger btn-block">Заказать звонок</a>
+                        </div>
                     </div>
-                    <div class="row">    
+                    <div class="row">
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                              <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                              </li>
+                            </ul>
+                        </div>    
                     </div>
                 </div>
             </div>
