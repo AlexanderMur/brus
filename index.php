dffb3684840af4fb3df037a26d424e4cc2f09e8e
sanyamensky <sashko098@gmail.com>
2017-10-25 13:03:12 +0500

    index

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
     <title>Home</title>
 </head>
 <body>
-
+    aaaaa
+    dlpasdl
     <img src="<?php echo get_the_post_thumbnail_url() ?>" data-toggle="tooltip" data-placement="right" title="<?php the_title() ?>">
     <script src="<?php echo get_template_directory_uri() . '/' ?>js/main.js"></script>
 </body>
